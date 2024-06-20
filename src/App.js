2649ce1d0f766da2c73f48c58852fac69e22e016
ramkishorepandey <ramkishorepandey1945@gmail.com>
2024-06-20 11:45:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Lords Picture</h1>
       <h2>Hello Nirmal Kumar Testing</h2>
+      <h3>Code Added by Nirmal Kumar <h3>
       <img alt='' src="./images/Brahma.jpeg"></img>
       <img alt='' src="./images/Krishna.jpeg"></img>
       <img alt='' src="./images/ram.jpeg"></img>
